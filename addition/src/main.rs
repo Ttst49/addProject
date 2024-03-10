5b59364c3cd1fc2a33dfcd5913bff42358f99c7a
Ttst <thibautstachnick@gmail.com>
2024-03-10 22:48:06 +0100

    add a way to test add_one crate in addition file

--- addition/src/main.rs
+++ addition/src/main.rs
@@ -1,3 +1,11 @@
+use add_one;
+
+
 fn main() {
-    println!("Hello, world!");
+    let nombre = 10;
+    println!(
+        "Hello world! {} plus one equal {}",
+        {nombre},
+        {add_one::add_one(nombre)}
+    )
 }
